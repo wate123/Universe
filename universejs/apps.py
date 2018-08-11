5a7929832b8c2d670bc36dd3f1c0eedb4cd79ee1
Jun Lin <junlin0604@gmail.com>
2018-08-11 01:33:14 -0500

    Initial commit

---
+++ universejs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UniversejsConfig(AppConfig):
+    name = 'universejs'
---
+++ universejs/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'universejs/index.html'
